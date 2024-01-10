3860a3ce78fae71cdd747f3de80c40e8bfe42c83
Cauê Lima <cauesvlima@gmail.com>
2024-01-09 21:11:19 -0300

    Update Script_Banco_tbaluno.sql

--- _Carometro/_Carometro/Script Banco/Script_Banco_tbaluno.sql
+++ _Carometro/_Carometro/Script Banco/Script_Banco_tbaluno.sql
@@ -62,19 +62,7 @@ CREATE TABLE `tb_aluno` (
 -- Despejando dados para a tabela `tb_aluno`
 --
 
-INSERT INTO `tb_aluno` (`Rm`, `Cpf`, `Rg`, `Sobrenome`, `Nome`, `Nacionalidade`, `DtNasc`, `Sexo`, `Foto`, `Responsavel`, `CelularResp`, `Logradouro`, `NomeLog`, `Numero`, `Bairro`, `Cidade`, `Cep`, `Uf`, `Pais`, `Complemento`, `FoneFixo`, `Celular`, `Email`) VALUES
-(210465, '123.456.789-79', '10.210.699-X', 'FERRAÇO', 'RICARDO SANTANA', NULL, '2005-01-01', 'M', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'ricardo@ricardo.com.br'),
-(210468, '123.456.789-79', '10.210.699-X', 'CAMARGO', 'PEDRO HENRIQUE FABRE DE', NULL, '2005-12-23', 'M', '', 'Pedro pai', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'pedroso@pedro.com.br'),
-(210469, '123.456.789-79', '10.210.699-X', 'ROSA', 'MIGUEL SACARDO LALLA', NULL, '2005-12-08', 'M', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'miguelzinho@miguel.com.br'),
-(210472, '123.456.789-79', '10.210.699-X', 'JACOB', 'LORENNA NICOLE ALVES', NULL, '2005-02-17', 'F', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(210473, '123.456.789-79', '10.210.699-X', 'LIMA', 'CAUÊ SILVEIRA VAZ DE', NULL, '2005-11-07', 'M', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(210475, '123.456.789-79', '10.210.699-X', 'DOCEMA', 'FERDINANDO MARTINS', NULL, '2004-02-02', 'M', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(210480, '123.456.789-79', '10.210.699-X', 'SOUZA', 'BRAYAN JUNIOR DE', NULL, '2005-12-08', 'M', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'brayanjunior925@gmail.com'),
-(210502, '123.456.789-79', '10.210.699-X', 'ARDITO', 'BIANCA GONÇALVES', NULL, '2006-06-14', 'F', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'bianquinha@etec.sp.gov.br'),
-(210547, '123.456.789-79', '10.210.699-X', 'MORANDIN', 'RENÃ BORTOLOTTI', 'BRASILEIRO', '0000-00-00', 'm', 'elefante.jpg', 'RENã PAI', '19955446633', 'rua', 'sete de setembro', 299, 'Centro', 'Monte Alegre do Sul', '13.820-000', 'sp', 'Brasil', 'casa', '1912349876', '1999117485', 'rena@etec.sp.gov.br'),
-(210630, '123.456.789-79', '10.210.699-X', 'CABRAL', 'CAIO HENRIQUE DA SILVA', '', '0000-00-00', 'm', '', 'Shirley Mendonça da silva', '19955446644', 'rua', '', 0, 'Beira Rio', '', '', '', '', '', '', '', 'caio@caio.com.br'),
-(767676, '123.456.789-09', '11.111.111', 'rodrigues', 'claudia de jesus', '', '0000-00-00', 'f', NULL, '', '', '', '', 0, '', '', '', '', '', '', '', '', '');
-COMMIT;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
